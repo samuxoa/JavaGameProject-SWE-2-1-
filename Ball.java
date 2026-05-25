8f7720f33b3903375ab63b0ab9e5c23dca101218
Samia Rahman <samurahman001@gmail.com>
2026-05-25 13:20:13 +0600

    Upload game files

---
+++ Ball.java
@@ -0,0 +1,21 @@
+public class Ball {
+    int x = 120;
+    int y = 350;
+    int xDir = -1;// left side
+    int yDir = -2; // up side
+
+    int size = 20;// diameter of ball
+
+    public void move() {
+        x += xDir;
+        y += yDir; // atate move krbe location change hbe
+    }
+
+    public void reverseX() {
+        xDir = -xDir; // akhn right side e jbe
+    }
+
+    public void reverseY() {
+        yDir = -yDir; // akhn down e jbe
+    }
+}

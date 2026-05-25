abe26364bbb5396aec9e384076f7ee7f29d4eb51
Samia Rahman <samurahman001@gmail.com>
2026-05-25 14:53:15 +0600

    Paddle code upload

--- Paddle.java
+++ Paddle.java
@@ -1,3 +1,19 @@
 public class Paddle {
+    int x = 310;
+    int y = 550;
+    int width = 100;// paddle r length
+    int height = 10; // paddle r betdh
 
+    public void moveLeft() {
+        if (x > 10) {
+            x -= 20; // padle bame e shortese
+        }
+    }
+
+    public void moveRight() {
+        if (x < 580) {
+            x += 20;// paddle dane shortese
+        }
+    }
+    // akhne 580 die jate boarder e touch nh kre ata ensure kra hoise
 }
